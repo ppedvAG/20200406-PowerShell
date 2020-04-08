5f727fc607df3194acb91f130e65127cf8f16912
Stefan Ober <StefanO@ppedv.de>
2020-04-08 14:04:17 +0200

    Skript-Parameter hinzugefügt

--- Skript/LogonEvents.ps1
+++ Skript/LogonEvents.ps1
@@ -1,4 +1,8 @@
-﻿
-
+﻿[cmdletBinding()]
+Param(
+[string]$ComputerName,
+[int]$EventId,
+[int]$Newest 
+)
 #Abfrage des EventLog Security und anschließendes Filtern auf die Eventid 4624 (AnmeldeEvent)
-Get-EventLog -LogName Security -ComputerName localhost | Where-Object EventID -eq 4624 | Select-Object -First 5
+Get-EventLog -LogName Security -ComputerName $ComputerName | Where-Object EventID -eq $EventId | Select-Object -First $Newest 
