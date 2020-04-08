f8bf4f4dc996c18b769a07e5d906c7d5393fa5fc
Stefan Ober <StefanO@ppedv.de>
2020-04-08 13:56:57 +0200

    Kommentar hinzugefügt

--- Skript/LogonEvents.ps1
+++ Skript/LogonEvents.ps1
@@ -1,3 +1,4 @@
 ﻿
 
+#Abfrage des EventLog Security und anschließendes Filtern auf die Eventid 4624 (AnmeldeEvent)
 Get-EventLog -LogName Security -ComputerName localhost | Where-Object EventID -eq 4624 | Select-Object -First 5
