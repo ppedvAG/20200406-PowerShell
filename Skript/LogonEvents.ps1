13f3f943d0b2577b1eaf3cc432d1f079d9548e97
Stefan Ober <StefanO@ppedv.de>
2020-04-08 14:08:53 +0200

    Skript-Parameter COmputername als Pflicht PArameter, Rest mit Standardwerten versehen

--- Skript/LogonEvents.ps1
+++ Skript/LogonEvents.ps1
@@ -1,8 +1,9 @@
 ﻿[cmdletBinding()]
 Param(
+[Parameter(Mandatory=$true)]
 [string]$ComputerName,
-[int]$EventId,
-[int]$Newest 
+[int]$EventId = 4624,
+[int]$Newest = 5
 )
 #Abfrage des EventLog Security und anschließendes Filtern auf die Eventid 4624 (AnmeldeEvent)
 Get-EventLog -LogName Security -ComputerName $ComputerName | Where-Object EventID -eq $EventId | Select-Object -First $Newest 
