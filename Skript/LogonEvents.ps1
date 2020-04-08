0e8296683c5a8c720af22cfb82f4d4b40524459d
Stefan Ober <StefanO@ppedv.de>
2020-04-08 14:12:51 +0200

    Skript Verbose Ausgabe hinzugefügt

--- Skript/LogonEvents.ps1
+++ Skript/LogonEvents.ps1
@@ -5,5 +5,11 @@ Param(
 [int]$EventId = 4624,
 [int]$Newest = 5
 )
+
+Write-Verbose -Message "Vor Abfrage es wurden folgende Werte verwendet:
+Computername: $ComputerName
+EventID: $EventID
+Newest: $Newest
+"
 #Abfrage des EventLog Security und anschließendes Filtern auf die Eventid 4624 (AnmeldeEvent)
 Get-EventLog -LogName Security -ComputerName $ComputerName | Where-Object EventID -eq $EventId | Select-Object -First $Newest 
